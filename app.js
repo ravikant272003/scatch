10fb73f28c80cb0a2e2c49e5c0c0f1881582763d
ravikant <ravikant272003@gmail.com>
2025-09-17 21:37:20 +0530

    n

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express=require('express')
 const app=express()
-
 const cookieParser=require("cookie-parser")
 const path =require("path")
+
+const db=require('./config/mongoose-connection')
+const ownersRouter = require('./routes/ownersRouter')
+const productsRouter=require('./routes/productsRouter')
+const usersRouter=require('./routes/usersRouter')
+
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
@@ -10,8 +15,8 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname,"public")))
 app.set('view engine',"ejs")
 
-app.get('/',(req,res)=>{
-    res.send('helloo')
-})
+app.use("/owners",ownersRouter)
+app.use("/products",productsRouter)
+app.use("/users",usersRouter)
 
 app.listen(3000)
