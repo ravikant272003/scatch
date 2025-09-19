c01fc6b6f4af3b19d659feaf55f5927748d080af
ravikant <ravikant272003@gmail.com>
2025-09-19 16:58:34 +0530

    change

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const db=require('./config/mongoose-connection')
 const ownersRouter = require('./routes/ownersRouter')
 const productsRouter=require('./routes/productsRouter')
 const usersRouter=require('./routes/usersRouter')
+const indexRouter=require('./routes/index')
 
 
 app.use(express.json())
@@ -18,5 +19,6 @@ app.set('view engine',"ejs")
 app.use("/owners",ownersRouter)
 app.use("/products",productsRouter)
 app.use("/users",usersRouter)
+app.use('/',indexRouter)
 
 app.listen(3000)
